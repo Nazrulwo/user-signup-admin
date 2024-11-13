81cdff4ba0e12e57e8649bb30bee4e78b328981b
nazrul <nazrulwo@gmail.com>
2024-11-13 15:34:13 +0100

    Fix dashboard

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header";
+import styles from "../styles/GlobalStyles";
+import Sidebar from "./Sidebar";
+import PreviousVisitor from "./PreviousVisitor";
+
+const Dashboard: React.FC = () => {
+  return (
+    <>
+      <Header />
+      <Sidebar />
+      <div style={styles.content}>
+        <PreviousVisitor />
+      </div>
+    </>
+  );
+};
+
+export default Dashboard;
